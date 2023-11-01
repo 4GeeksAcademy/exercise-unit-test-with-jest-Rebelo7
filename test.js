48c061b93842ebffc3735e95a7a6ef74dc066870
Rebelo7 <143350138+Rebelo7@users.noreply.github.com>
2023-11-01 22:42:02 +0000

    adds functions and tests

--- test.js
+++ test.js
@@ -12,7 +12,7 @@ test("One euro should be 1.07 dollars", function() {
     expect(fromEuroToDollar(3.5)).toBe(3.745); 
 })
 
-test2("One dollar should be 1.075 yens", function() {
+test("One dollar should be 156.5 yens", function() {
     
     const { fromDollarToYen } = require('./app.js');
 
@@ -20,22 +20,22 @@ test2("One dollar should be 1.075 yens", function() {
     const yens = fromDollarToYen(5);
 
    
-    const expected = 5 * 0.93; 
+    const expected = 4.65 * 156.5; 
     
     
-     expect(fromDollarToYen(5)).toBe(4.65); 
+     expect(fromDollarToYen(5)).toBe(727.725); 
 })
 
-test3("One yen should be 1.149", function() {
+test("One yen should be 0.00522 pounds", function() {
 
     const { fromYenToPound } = require('./app.js');
 
    
-    const pounds = fromYenToPound(4);
+    const pounds = fromYenToPound(200);
 
    
-    const expected = 4 * 0.87; 
+    const expected = 1.2 * 0.87; 
     
     
-     expect(fromYenToPound(4)).toBe(3.48); 
+     expect(fromYenToPound(200)).toBe(1.044); 
 })
